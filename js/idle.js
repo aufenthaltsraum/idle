b1d91df9ec9ebe6cf1dfee4e2159ce456cea95ef
Aras Ergus <alpha.ergus@gmail.com>
2015-02-28 00:21:49 +0100

    Reshape the room object

--- js/idle.js
+++ js/idle.js
@@ -1,5 +1,6 @@
 (function () {
   var app = angular.module('idle', []);
+  room = new Room();
     // filters
   app.filter('floor', function (){ 
     return function (input){
@@ -26,53 +27,38 @@
   });
     // controllers
   app.controller ('IdleController',  function ($interval) {
-    room = new Room();
     this.room = room;
     //
     var ref = this.room;
     
     this.tick = function () {
-      var update = new Date ();
+      var i = 0;
+      var update = new Date();
       var delta = update - ref.update;
+      for (i = 0; i < ref.ticks.length; i++) {
+         ref.ticks[i].fct(ref, delta);
+      }
       ref.update = update;
-      ref.drinking -= delta/1000;
-      var i;
-      for (i = 0; i < ref.people.who.length; i++) {
-        if (ref.people.who[i].occupation === "exc") {
-          ref.exercises += ref.people.who[i].tick(delta) *((ref.drinking>0)?5:1);
-        }
-        else if (ref.people.who[i].occupation === "tea") {
-          ref.tea += ref.people.who[i].tick(delta);
-        }
-        else {
-          window.alert ("occupation " + ref.people.who[i].occupation + " not known in function IdleControler.tick");
-        }
-      };
     }
     $interval(this.tick,100);
-    this.text = function (o) {
-      var i;
-      for (i = 0; i < ref.occupationList.length; i++) {
-        if (o === ref.occupationList[i][0])
-          return ref.occupationList[i][1];
-      };
-      return undefined;
-    };
     this.drink = function (){
-      if ((!(ref.drinking>0))&&ref.tea>0) {
-        ref.tea--;
+      if (!(ref.tea.active) && ref.tea.amount > 0) {
+        ref.tea.amount--;
+        ref.tea.active = true;
+        ref.tea.time = 60000;
         ref.cups++;
-        ref.drinking = 60;
+        ref.ticks.push(ref.tea.tick);
       }
     }
   });
   
   app.controller("ChangeOccupationCtrl", function () {
     this.occupation = "Choose a new Occupation";
-    this.occupations = ["exc","tea"];
+    this.occupations = room.occupations;
     var ref = this;
-    this.changeOccupation = function (person) {
-      person.occupation = ref.occupation[0];
+    this.changeOccupation = function (person, tag) {
+      person.occupation = ref.occupation;
+      room.findTick("person" + person.id).fct = ref.occupation.tickFunction;
       ref.occupation = "Choose a new Occupation";
     };
   });
