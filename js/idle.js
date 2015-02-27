a0a996fbf5bb05073b17469ecc785b7fd8ea0290
Shaomada <stefan.rabenstein@web.de>
2015-02-27 18:47:39 +0100

    Reanabled buying tea
    Some stuff was done including a filter and a list in the
    idleController for aliases and plaintext of occupation.
    Unfortunatly the filter does not use the list, so both must
    be updated as of now if occupations are changed

--- js/idle.js
+++ js/idle.js
@@ -18,7 +18,11 @@ var Person = function (o, nextId) {
     if (ref.occupation === "exc") {
       return delta/1000;
     }
+    else if (ref.occupation === "tea") {
+      return delta/100000;
+    }
     else {
+      console.alert ("occupation " + ref.people.who[i].occupation + " not known in function Person.tick");
       return undefined;
     }
   };
@@ -32,6 +36,8 @@ var Person = function (o, nextId) {
     this.tea = 5;
     this.update = new Date();
     this.people = new People(["exc", "exc"]);
+    this.occupationList = [["exc","solving exercises"],["tea","buying tea"]];
+    //
     var ref = this;
     this.tick = function () {
       var update = new Date ();
@@ -42,18 +48,56 @@ var Person = function (o, nextId) {
         if (ref.people.who[i].occupation === "exc") {
           ref.exercises += ref.people.who[i].tick(delta);
         }
+        else if (ref.people.who[i].occupation === "tea") {
+          ref.tea += ref.people.who[i].tick(delta);
+        }
         else {
-          ; // buying tea, ...
+          window.alert ("occupation " + ref.people.who[i].occupation + " not known in function IdleControler.tick");
         }
       };
     }
     $interval(this.tick,100);
+    this.text = function (o) {
+      var i;
+      for (i = 0; i < ref.occupationList.length; i++) {
+        if (o === ref.occupationList[i][0])
+          return ref.occupationList[i][1];
+      };
+      return undefined;
+    };
   });
   
   app.filter('floor', function (){ 
     return function (input){
+      return Math.floor(input); 
+    }
+  });
+  app.filter('ceil', function (){ 
+    return function (input){
       return Math.ceil(input); 
     }
+  });
+  app.filter('occupationText', function (){ 
+    return function (input){
+      if (input == "exc") {
+        return "solving exercises";
+      }
+      else if (input == "tea") {
+        return "buying tea";
+      }
+      else {
+        return undefined;
+      }
+    }
+  });
+  app.controller("OccupationSelectController", function () {
+    this.occupation = "Choose a new Occupation";
+    this.occupations = ["exc","tea"];
+    var ref = this;
+    this.changeOccupation = function (person) {
+      person.occupation = ref.occupation[0];
+      ref.occupation = "Choose a new Occupation";
+    };
   });
 })();
   
